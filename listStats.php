f5ea00a5a4c2fdd8c233a134036fd1b0c3122333
alarichadef <alarichadef@gmail.com>
2015-06-16 00:27:55 +0200

    correction stats

--- listStats.php
+++ listStats.php
@@ -18,8 +18,8 @@ include('menu.php');
 		$query = 
 			@"select projet, avg(montant) as moyenne from depense group by (projet) having avg(montant) > (select avg(montant) from depense)";
 
-		$result = pg_query($query);
-while($result = pg_fetch_array($requests)){
+		$results = pg_query($query);
+while($result = pg_fetch_array($results)){
        echo "<tr><td>".$result['projet']."</td><td>".$result['montant']."</td><td>".$result['fin']."</td></tr>";
 
 }
@@ -37,8 +37,8 @@ while($result = pg_fetch_array($requests)){
 		$query = 
 			@"select membre_projet.mail, nom, count(projet) as nombre from membre_projet, personne where personne.mail = membre_projet.mail group by membre_projet.mail, nom order by nombre desc";
 
-		$result = pg_query($query);
-while($result = pg_fetch_array($requests)){
+		$results = pg_query($query);
+while($result = pg_fetch_array($results)){
        echo "<tr><td>".$result['membre_projet.mail']."</td><td>".$result['nom']."</td><td>".$result['nombre']."</td></tr>";
 
 }
@@ -58,8 +58,8 @@ while($result = pg_fetch_array($requests)){
 		$query = 
 			@" select count(mail) as nombre, membre_projet.projet as p, appel_a_projet.description as d from proposition_de_projet,appel_a_projet,projet,membre_projet where membre_projet.projet = projet.id and projet.proposition = proposition_de_projet.id and proposition_de_projet.appel_a_projet = appel_a_projet.id group by membre_projet.projet, appel_a_projet.description;";
 
-		$result = pg_query($query);
-while($result = pg_fetch_array($requests)){
+		$results = pg_query($query);
+while($result = pg_fetch_array($results)){
        echo "<tr><td>".$result['membre_projet.projet']."</td><td>".$result['d']."</td><td>".$result['nombre']."</td></tr>";
 
 }
