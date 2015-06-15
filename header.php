08e93d29068c0f424c080d22b0a05a79dbec1c6f
alarichadef <alarichadef@gmail.com>
2015-06-16 01:01:58 +0200

    correction header menu

--- header.php
+++ header.php
@@ -24,12 +24,40 @@
                 </div>
                 <nav class="collapse navbar-collapse" role="navigation">
                     <ul class="nav navbar-nav">
-                        <li>
-                            <a href="#sec">Projets </a>
-                        </li>
-                        <li>
-                            <a href="#sec">Statistiques</a>
-                        </li>
+                        
+				<?php
+				if (isset($_SESSION['role'])){
+					if($_SESSION['role'] == 'membre_du_laboratoire'){
+					?>
+						<li><a href="listRequest.php">Liste des appels à projets</a></li>
+						<li><a href="listProposals.php">Liste des propositions de projet</a></li> 
+						<li><a href="financerDashboard.php">Liste des Financeurs</a></li>
+						<li><a href="listProjects.php">Liste des Projets</a></li>
+						<li><a href="listExpenses.php">Liste des dépenses</a></li>
+						<li><a href="listStats.php">Liste des statistiques</a></li>
+					<?php
+					}
+					if($_SESSION['role'] == 'webmaster'){
+					?>
+						<li><a href="listRequest.php">Liste des appels à projets</a></li>
+						<li><a href="listProposals.php">Liste des propositions de projet</a></li> 
+						<li><a href="financerDashboard.php">Liste des Financeurs</a></li>
+						<li><a href="listProjects.php">Liste des Projets</a></li>
+						<li><a href="listExpenses.php">Liste des dépenses</a></li>
+						<li><a href="listStats.php">Liste des statistiques</a></li>
+					<?php
+					}
+					if($_SESSION['role'] == 'employe_de_contact'){
+					?>
+<!-- 						<li><a href="listRequest.php">Liste des appels à projets</a></li>
+ -->					<!-- <li><a href="contactEmployeDashboard.php#projets">Liste des propositions de projet</a></li>  -->
+						<li><a href="contactEmployeDashboard.php#financeurs">Liste des Financeurs</a></li>
+						<li><a href="contactEmployeDashboard.php#projets">Liste des Organismes de Projet</a></li>
+						<li><a href="listExpenses.php">Liste des dépenses</a></li>
+					<?php
+					}
+				}
+				?>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="login.php">Deconnexion</a></li>
