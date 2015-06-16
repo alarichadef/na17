a841625a609b495383f9b9c6bf70b7f4df960e89
alarichadef <alarichadef@gmail.com>
2015-06-16 12:12:22 +0200

    update stat

--- listStats.php
+++ listStats.php
@@ -67,6 +67,110 @@ while($result = pg_fetch_array($results)){
                         </tbody>
                     </table>
 
+<h2>Nombre de projets, de propositions de projets et d'appels a projets</h2>
+<table class="table table-bordered table-hover table-striped">
+    <thead><tr><th>Nombre de projets</th><th>Nombre de propositions</th><th>Nombre d'appels a projet</th></tr></thead>
+                        <tbody>
+    <?php 
+		$db = dpconnexion();
+		$query = 
+			@" select count(id) as c from projet";
+
+		$results = pg_query($query);
+		$result = pg_fetch_array($results);
+		$query = 
+			@" select count(id) as c from proposition_de_projet";
+		$results1 = pg_query($query);
+		$result1 = pg_fetch_array($results1);
+		$query = 
+			@" select count(id) as c from appel_a_projet";
+		$results2 = pg_query($query);
+		$result2 = pg_fetch_array($results1);
+
+
+       echo "<tr><td>".$result['c']."</td><td>".$result1['c']."</td><td>".$result2['c']."</td></tr>";
+
+?>
+                        </tbody>
+                    </table>
+
+
+
+<h2>Budget moyen de toutes les propositions de projets</h2>
+<table class="table table-bordered table-hover table-striped">
+    <thead><tr><th>Moyenne</th></tr></thead>
+                        <tbody>
+    <?php 
+		$db = dpconnexion();
+		$query = 
+			@"SELECT AVG (psum) as m
+					FROM
+					(
+					    SELECT projet,sum(montant) AS psum FROM
+					    ligne_budgetaire
+					    GROUP BY projet
+					) as t,proposition_de_projet";
+
+		$results = pg_query($query);
+while($result = pg_fetch_array($results)){
+       echo "<tr><td>".$result['m']."</td></tr>";
+
+}
+?>
+                        </tbody>
+                    </table>
+
+<h2>Moyenne des depenses de tout les projets </h2>
+<table class="table table-bordered table-hover table-striped">
+    <thead><tr><th>Moyenne</th></tr></thead>
+                        <tbody>
+    <?php 
+		$db = dpconnexion();
+		$query = 
+			@"SELECT AVG (montant) as m
+					FROM
+					depense
+";
+
+		$results = pg_query($query);
+while($result = pg_fetch_array($results)){
+       echo "<tr><td>".$result['m']."</td></tr>";
+
+}
+?>
+                        </tbody>
+                    </table>
+
+
+
+
+<h2>Pourcentage des propositions de projets acceptes </h2>
+<table class="table table-bordered table-hover table-striped">
+    <thead><tr><th>Pourcentage</th></tr></thead>
+                        <tbody>
+    <?php 
+		$db = dpconnexion();
+		$query = 
+			@"SELECT  cast(pa as decimal(4,2))/cast(pp as decimal(4,2))*100 as p
+					FROM
+					(
+					    SELECT count(*) as pa from proposition_de_projet where acceptation = true
+					) as t,(
+					    SELECT count(*) as pp from proposition_de_projet
+					) as t1
+
+
+";
+
+		$results = pg_query($query);
+while($result = pg_fetch_array($results)){
+       echo "<tr><td>".$result['p']."</td></tr>";
+
+}
+?>
+                        </tbody>
+                    </table>
+
 
 <?php
 dpdisconnect();
